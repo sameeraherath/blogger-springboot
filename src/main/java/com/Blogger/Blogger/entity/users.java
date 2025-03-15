7dfa76bc386682479b140966b74846fe30517d20
sameeraherath <sameerastar6@gmail.com>
2025-03-15 07:44:41 +0530

    first commit

---
+++ src/main/java/com/Blogger/Blogger/entity/users.java
@@ -0,0 +1,15 @@
+package com.Blogger.Blogger.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name="users")
+@Data
+
+
+public class users {
+
+
+}
